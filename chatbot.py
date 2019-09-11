cf2a815cd981d924e29bc2ee4a5678ea18bd9ba8
David <gohzhiwei@hotmail.com>
2019-09-11 17:18:49 +0800

    Make selection work
    Yay

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ import random
 import re
 import json
 
+DEBUG = 0
 
 # Functions that don't need other inits
 def rand_response(response_list):
@@ -45,11 +46,30 @@ def format_text(text):
         text = text.replace(pair[0],pair[1])
     return text
 
+PLAN_DICT = {"products":{
+        "p_01": {
+            "price": 100,
+            "desc": "Basic plan A"
+        },
+        "p_02": {
+            "price": 150,
+            "desc": "More advanced plan. Plan A + feature"
+        },
+        "p_03": {
+            "price": 9001,
+            "desc": "Extremely comprehensive plan C. C for full Coverage"
+        },
+        "ERR":{
+            "price":0,
+            "desc": "Something went wrong"
+        }
+    }
+}
+
 def parse_plan_selection(msg):
     msg = msg.replace("plan", "")
     msg = msg.replace(" ", "")
     selection = "ERR"
-    print("m",msg)
     if re.fullmatch('a|1', msg):
         selection = "p_01"
     elif re.fullmatch('b|2', msg):
@@ -57,7 +77,10 @@ def parse_plan_selection(msg):
     elif re.fullmatch('c|3', msg):
         selection = "p_03"
 
-    return selection
+    product = PLAN_DICT["products"][selection]
+    s_data = (selection, product["price"], product["desc"])
+
+    return s_data
 
 # Big Chatbot class
 class Chatbot():
@@ -99,7 +122,7 @@ class Chatbot():
         print(reply)
         return
 
-
+    # Generates a pure reply as in a text
     def generate_reply(self, reply_key, chat):
         REPLY_DATABASE = self.REPLY_DATABASE
         CONTEXT_REPLY_STATES = self.CONTEXT_REPLY_STATES
@@ -116,10 +139,11 @@ class Chatbot():
         curr_state = chat.get_state()
         if curr_state in CONTEXT_REPLY_STATES:
             context_info = chat.get_selection()
-            print("ctxt",context_info)
+            if DEBUG: print("ctxt",context_info)
             msg_template = reply_key # TODO have proper message template lookups instead of hardcoded reply key
             # construct message
-            replytext = msg_template.format(context_info)
+            name, price, desc = context_info
+            replytext = msg_template.format(name, desc, price)
             # print("replytxt", replytext)  
             return replytext
 
@@ -142,6 +166,7 @@ class Chatbot():
 
         return -1
 
+    # Returns a text reply
     def get_reply_to_msg(self, chat, msg):
         GENERAL_INTENT_LIST = self.GENERAL_INTENT_LIST
         POLICY_RULES = self.POLICY_RULES
@@ -186,7 +211,8 @@ class Chatbot():
             intent = get_special_intent(curr_state,p_msg)
             if not intent:
                 intent = get_general_intent(p_msg)
-            print("Intent is:",intent,"\n")
+            
+            if DEBUG: print("Intent is:",intent,"\n")
 
             return intent
 
@@ -233,17 +259,22 @@ class Chatbot():
 
         # print("intent",intent)
         new_state, reply_key = intent_to_reply(curr_state, intent)
-        reply = self.generate_reply(reply_key,chat)
 
         # TODO: Better software engine practice
+        # Change state
         self.change_chat_state(chat, new_state)
+        # Get reply
+        reply = self.generate_reply(reply_key,chat)
         chat.set_prev_msg(reply)
 
-        # reply = reply_key
-        
-        return (reply, curr_state)
+        if DEBUG:
+            return (reply, curr_state)
+
+        return reply
 
     def change_chat_state(self, chat, new_state, selection = -1):
+
+        # Go to previous state
         if new_state == self.PREV_STATE_FLAG:
             new_state = chat.get_prev_state()
 
@@ -319,7 +350,7 @@ class Chatbot():
             (STATES['choose_plan'], INTENTS['confusion']): (STATES['sales_query'], "Oh, let me clarify the plans."),
             (STATES['choose_plan'], INTENTS['affirm']): (STATES['choose_plan'], "Great! But you still have to choose a plan."),
             (STATES['choose_plan'], INTENTS['indicate_plan']): (STATES['confirm_plan'], "Just to confirm, your have selected is {0}. \n Description:\n{1}"),
-            (STATES['confirm_plan'], INTENTS['affirm']): (STATES['payment'], "That will be ${0}!"),
+            (STATES['confirm_plan'], INTENTS['affirm']): (STATES['payment'], "That will be ${2}!"),
             (STATES['payment'], INTENTS['affirm']): (STATES['finish_sale'], "Success! Thank you for using SHEBAO! Is there anything else I can help with?"),
             (STATES['finish_sale'], INTENTS['affirm']): (STATES['init'], "sales_pitch"),
             (STATES['finish_sale'], INTENTS['deny']): (STATES['goodbye'], "Bye! Hope to see you again soon!"),
@@ -351,7 +382,7 @@ class Chat:
         self.prev_messages = []
 
     def change_state(self,new_state):
-        print("changing to", new_state)
+        if DEBUG: print("changing to", new_state)
         self.prev_state = self.state
         self.state = new_state
 
@@ -373,8 +404,7 @@ class Chat:
         self.selection = selection
     
     def get_selection(self):
-        print("selcslad",self.selection)
-        return tuple(self.selection)
+        return self.selection
 
     def clear_selection(self):
         self.selection = False
