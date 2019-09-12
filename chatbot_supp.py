00d0aa9315c8480bac0381d0f7c81d92c120b354
David <gohzhiwei@hotmail.com>
2019-09-12 11:06:07 +0800

    Create Message class

--- chatbot_supp.py
+++ chatbot_supp.py
@@ -1,6 +1,35 @@
-DEBUG = 0
+import cbsv
+
+DEBUG = 1
+
 
 # Have a message class? Or some sort of flag for messages. Indicate state-changing messages.
+
+# A packet that has info about state and message
+# State-Message Packet
+PREV_STATE_F = "299 PREV_STATE"
+PREV_STATE_MSG = "prev_state_msg"
+class SMP:
+    def __init__(self, msg, state):
+        self.msg = msg
+        self.state = state
+        self.stateChange = True
+        self.backtrack = False
+
+    def set_backtrack(self):
+        self.backtrack = True
+        self.stateChange = True #?
+
+    def set_actions(self, action, pending_act = None):
+        self.action = action
+        self.pending_act = pending_act
+
+    @classmethod
+    def go_back_state(cls):
+        obj = cls(PREV_STATE_MSG, PREV_STATE_F)
+        obj.backtrack = True
+        obj.stateChange = False
+        return obj
 
 # A product???
 class Product:
@@ -21,19 +50,27 @@ class Chat:
         self.state = initial_state
         self.convo_history = convo_history
         self.prev_messages = []
-        self.frame_history = []
+        self.frame_history = [initial_state,]
     def change_state(self,new_state):
+        # If state never change, do nothing
+        if new_state == self.state or new_state == cbsv.PREV_STATE_FLAG():
+            return
         if DEBUG: print("changing to", new_state)
-        self.prev_state = self.state
+        self.frame_history.append(self.state)
         self.state = new_state
+        self.first_state_pop = True
 
     def set_prev_msg(self, msg):
+        if msg == cbsv.DEFAULT_CONFUSED():
+            return
         self.prev_messages.append(msg)
         self.firstpop = True
     
     def pop_prev_msg(self):
         # TODO failsafe when empty?
         # Need to get the previous previous message
+        if len(self.prev_messages) < 2:
+            return self.prev_messages[-1]
         if self.firstpop: 
             self.prev_messages.pop(-1)
             self.firstpop = False
@@ -43,7 +80,11 @@ class Chat:
         return self.state
 
     def get_prev_state(self):
-        return self.prev_state
+        if len(self.frame_history) < 1:
+            return frame_history[-1]
+        #     self.first_state_pop = False
+        #     self.frame_history.pop(-1)
+        return self.frame_history.pop(-1)
 
     def set_selection(self,selection):
         self.selection = selection
