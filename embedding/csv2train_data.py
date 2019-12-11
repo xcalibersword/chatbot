d679254cc9f6a3bd7e610c4287891779a9b5a47a
wang jun jie <wang.jun.jie01@hotmail.com>
2019-12-11 17:19:52 +0800

    Tweak embedding

--- embedding/csv2train_data.py
+++ embedding/csv2train_data.py
@@ -5,21 +5,21 @@ import pandas as pd
 csv_list_list = []
 
 #insert path
-df = pd.read_csv(r"raw.csv",encoding="gb18030")
+df = pd.read_csv(r"raw.csv",encoding="gb18030",header=None)
 list_list = df.values.tolist()
 #read to dict
 a = {}
-label_list = list_list[3] # Actually the 5th row 
+label_list = list_list[0]
 num_intents = len(label_list)
 
-#intent example starts on the 6th row in excel (i=5 position when read)
+#intent example starts on the 2th row in excel (i=1 position when read)
 for i in range(num_intents):
     if str(label_list[i]) != "nan":
-        for l in list_list[4:]:
+        for l in list_list[1:]:
             if str(l[i]) != "nan":
                 csv_list_list.append([l[i],label_list[i]])
 
-#get intent-reply pairs
+# get intent-reply pairs
 # ans_list = list_list[2]
 # bot_resource = {}        
 # for intent,answer in zip(label_list,ans_list):
@@ -29,6 +29,6 @@ for i in range(num_intents):
 
 new_df = pd.DataFrame(data=csv_list_list)
 #insert path
-new_df.to_csv(r"generated_data.csv",index=False,encoding="gb18030")
+new_df.to_csv(r"generated_data.csv",index=0,header=0,encoding="gb18030")
 
 print("Done converting. Processed", str(num_intents),"intents")
